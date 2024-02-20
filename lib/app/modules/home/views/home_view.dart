eae06bece036a151d72cf6b4329167f437219c01
kayesAshfaq <asfak7074@gmail.com>
2024-02-20 13:47:47 +0600

    added skeleton of home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../../core/base/base_view.dart';
@@ -18,6 +19,79 @@ class HomeView extends BaseView<HomeController> {
 
   @override
   Widget body(BuildContext context) {
-    return const SizedBox();
+    return CustomScrollView(
+      slivers: [
+        // ----------------- image banner -----------------
+        const SliverToBoxAdapter(
+          child: ColoredBox(
+            color: Colors.yellow,
+            child: SizedBox(
+              height: 200,
+              child: Center(
+                child: Text('Banner'),
+              ),
+            ),
+          ),
+        ),
+
+        // text and view all button
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Text('Popular Items'),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text('View All'),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        // ----------------- grid view -----------------
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 548.h,
+            child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // number of items in each row
+                mainAxisSpacing: 8.0, // spacing between rows
+                crossAxisSpacing: 8.0, // spacing between columns
+                childAspectRatio: 248 / 160, // aspect ratio of each item (width/height)
+              ),
+              padding: const EdgeInsets.all(8.0), // padding around the grid
+              itemCount: 12, // total number of items
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.blue, // color of grid items
+                  child: Center(
+                    child: Text(
+                      'Item $index',
+                      style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return ListTile(
+                title: Text('Item $index'),
+                onTap: () {},
+              );
+            },
+            childCount: 20,
+          ),
+        ),
+      ],
+    );
   }
 }
