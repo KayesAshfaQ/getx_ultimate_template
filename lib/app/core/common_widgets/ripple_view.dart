18a718d90a95f60e1c64f6984f9a03d14c989b8b
kayesAshfaq <asfak7074@gmail.com>
2024-03-06 17:57:10 +0600

    docs: added documentation for RippleView widget

--- lib/app/core/common_widgets/ripple_view.dart
+++ lib/app/core/common_widgets/ripple_view.dart
@@ -4,15 +4,32 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../config/app_colors.dart';
 
 class RippleView extends StatelessWidget {
-  final Widget child;
-  final double elevation;
-  final double padding;
-  final bool isCircular;
-
-  /// child background color
-  final Color color;
-  final VoidCallback? onTap;
-
+  /// `RippleView` is a stateless widget that provides a ripple effect on tap.
+  ///
+  /// It takes a child widget and wraps it with a Material widget to provide the ripple effect.
+  /// The ripple effect is a visual feedback for user interaction.
+  ///
+  /// Parameters:
+  /// - `child`: The widget that you want to have the ripple effect.
+  /// - `elevation`: The z-coordinate at which to place this card. This controls the size of the shadow below the card.
+  /// - `padding`: The amount of space by which to inset the child.
+  /// - `isCircular`: A boolean value that determines if the shape of the widget is circular or not.
+  /// - `color`: The color to paint the background of the widget. The default color is white.
+  /// - `onTap`: A callback function that is called when the widget is tapped.
+  ///
+  /// Example usage:
+  /// ```dart
+  /// RippleView(
+  ///   child: Text('Hello World'),
+  ///   elevation: 2,
+  ///   padding: 8,
+  ///   isCircular: true,
+  ///   color: Colors.blue,
+  ///   onTap: () {
+  ///     print('Widget tapped!');
+  ///   },
+  /// );
+  /// ```
   const RippleView({
     super.key,
     required this.child,
@@ -22,6 +39,13 @@ class RippleView extends StatelessWidget {
     this.color = AppColors.white,
     this.onTap,
   });
+
+  final Widget child;
+  final double elevation;
+  final double padding;
+  final bool isCircular;
+  final Color color;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
