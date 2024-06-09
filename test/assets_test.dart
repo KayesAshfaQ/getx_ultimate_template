52dff550873338614c88b8041396d2e0a0e69cf3
kayesAshfaq <asfak7074@gmail.com>
2024-06-09 13:28:15 +0600

    refactor: Remove unused test files

--- test/assets_test.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:flutter_test/flutter_test.dart';
-import 'package:getx_ultimate_template/app/core/values/resources/resources.dart';
-
-void main() {
-  test('assets assets test', () {});
-}
